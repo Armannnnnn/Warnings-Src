1fcd2571b5e98917915526957a04da6b5693b740
Arman <7013133+Armannnnnn@users.noreply.github.com>
2023-02-26 15:55:01 -0500

    Create SetWarningsCommandCategories.java

---
+++ java/me/arman/warnings/commands/categories/SetWarningsCommandCategories.java
@@ -0,0 +1,78 @@
+package me.arman.warnings.commands.categories;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import me.arman.warnings.Main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SetWarningsCommandCategories implements CommandExecutor {
+
+	static Main plugin;
+
+	public SetWarningsCommandCategories(Main instance) {
+		plugin = instance;
+
+	}
+
+	public static boolean isInt(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("setwarnings")) {
+			if (!sender.hasPermission("warnings.set")) {
+				sender.sendMessage(ChatColor.RED + "You can not set warnings!");
+				return true;
+			}
+			if (args.length <= 2) {
+				sender.sendMessage(ChatColor.RED + "Usage: /setwarnings <player> <amount> <category>");
+				return true;
+			} else if (args.length >= 3) {
+				OfflinePlayer target = Bukkit.getServer().getOfflinePlayer(args[0]);
+				if (!isInt(args[1])) {
+					sender.sendMessage(ChatColor.RED + "Invalid warning number!");
+					return true;
+				}
+				int num = Integer.parseInt(args[1]);
+				if (num < 0) {
+					sender.sendMessage(ChatColor.RED + "You can not set negative warnings!");
+					return true;
+				}
+				if (!(plugin.getConfig().getConfigurationSection("categories")
+						.contains(args[2].replace(" ", "") + "."))) {
+					sender.sendMessage(ChatColor.RED
+							+ "That is an invalid warning category! Type /categories to view the valid reasons!");
+					return true;
+				}
+				plugin.dFile.getConfig().set(target.getUniqueId().toString() + ".username", target.getName());
+				plugin.dFile.getConfig().set(target.getUniqueId().toString() + ".warnings." + args[2], num);
+				if (plugin.getConfig().getBoolean("logSpecificInfo")) {
+					List<String> l = plugin.dFile.getConfig().getStringList(target.getUniqueId().toString() + ".info");
+					Date now = new Date();
+					SimpleDateFormat format = new SimpleDateFormat(plugin.getConfig().getString("dateFormat"));
+					l.add("Warnings set to " + "(" + num + ")" + " by " + "(" + sender.getName() + ")" + " in category "
+							+ "(" + args[2] + ")" + " at " + "(" + format.format(now) + ")");
+					plugin.dFile.getConfig().set(target.getUniqueId().toString() + ".info", l);
+				}
+				plugin.dFile.saveConfig();
+				sender.sendMessage(ChatColor.GREEN + "Set " + target.getName() + "\'s warnings to " + num
+						+ " in category " + args[2]);
+			}
+
+		}
+		return false;
+	}
+}
